412f09f9b5a41c0163eb7912ef68408aeca132da
Perry <petreli1@ymail.com>
2022-08-15 21:38:06 +0100

    added a port class + ships now accept port as a currentPort + tests

--- src/Ship.js
+++ src/Ship.js
@@ -1,11 +1,15 @@
 class Ship{
     constructor(startingPort) {
-        this.startingPort = startingPort
+        this.currentPort = startingPort
     };
 
     setSail() {
-        this.startingPort = false;
-    }
+        this.currentPort = false;
+    };
+
+    dock(port) {
+        this.currentPort = port
+    };
 };
 
 module.exports = { Ship }
--- src/__tests__/ship.test.js
+++ src/__tests__/ship.test.js
@@ -1,4 +1,17 @@
 const { Ship } = require('../Ship');
+const { Port } = require('../Port');
+
+describe('Port', () => {
+    it('can be instantiated', () => {
+        const port = new Port();
+        expect(port).toBeInstanceOf(Object)
+    });
+
+    it('can be named', () => {
+        const port = new Port('Dover');
+        expect(port.name).toEqual('Dover');
+    });
+});
 
 describe('Ship', () => {
     it('can be instantiated', () => {
@@ -6,14 +19,32 @@ describe('Ship', () => {
        expect(ship).toBeInstanceOf(Object) 
     });
 
-    it('has a starting port', () => {
-        const ship = new Ship('Dover');
-        expect(ship.startingPort).toEqual('Dover')
-    });
+    describe('Dock', () => {
+        it('starting point is an object', () => {
+            const port = new Port()
+            const ship = new Ship(port);
+            expect(ship.currentPort).toBeInstanceOf(Object);
+        });
 
-    it('can set sail', () => {
-        const ship = new Ship('Dover');
-        ship.setSail();
-        expect(ship.startingPort).toBeFalsy()
-    })
+        it('starting point has a name', () => {
+            const port = new Port('Dover');
+            const ship = new Ship(port);
+            expect(ship.currentPort.name).toEqual('Dover');
+        });
+
+        it('can set sail', () => {
+            const port = new Port('Dover');
+            const ship = new Ship(port);
+            ship.setSail();
+            expect(ship.currentPort).toBeFalsy();
+        });
+
+        it('can dock at a new port', () => {
+            const port = new Port('Dover');
+            const ship = new Ship(port);
+            const calais = new Port('Calais')
+            ship.dock(calais);
+            expect(ship.currentPort.name).toEqual('Calais')  
+        });
+    });
 });
