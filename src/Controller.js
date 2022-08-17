e383c355aea3707fa1ccc556b62227de3e6d7ad9
Perry <petreli1@ymail.com>
2022-08-17 15:58:18 +0100

    added ability to render ports in sea

--- src/Controller.js
+++ src/Controller.js
@@ -18,6 +18,23 @@
                 index += 1;
             }, 1000);
         };
+
+        renderPorts(ports) {
+            const portsContainer = document.getElementById('ports');
+            let portsWidth = 0;
+
+            ports.forEach((port, i) => {
+                const portElement = document.createElement('div');
+                portElement.className = 'port';
+                portElement.dataset.portName = port.name;
+                portElement.dataset.portIndex = i;
+
+                portsWidth += 256;
+                portsContainer.style.width = `${portsWidth}px`
+
+                portsContainer.appendChild(portElement);
+            });
+        };
     };
 
     if (typeof module !== 'undefined' && module.exports) {
@@ -26,20 +43,3 @@
         window.Controller = Controller;
     }
 }());
-
-
-
-const initialiseSea = () => {
-    const backgrounds = [
-        './images/water0.png',
-        './images/water1.png'
-    ];
-
-    const viewport = document.getElementById('viewport');
-    let index = 0;
-
-    setInterval(() => {
-        viewport.style.backgroundImage = `url(${backgrounds[index % backgrounds.length]})`;
-        index += 1;
-    }, 1000);
-};
